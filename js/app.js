d4f2cec9d609459da9f21f282704c41759844d87
Abdalrhman8000 <abdo.eslam2021@gmail.com>
2021-01-04 05:57:10 +0200

    NewUploead

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//nav Background Change
+//nav-Background-Change
 
 window.addEventListener('scroll',function(){
     let nav = document.querySelector("nav");
@@ -8,30 +8,105 @@ window.addEventListener('scroll',function(){
 
 
 window.addEventListener('scroll',function(){
-    let sec2 = document.querySelector(".sec-2");
+    let sec2 = document.querySelector(".item-1");
+    let win = window.scrollY > 300;
+    sec2.classList.toggle('sec-2-2-1',win)
+})
+
+
+//Sec-2-Animation-Start
+
+window.addEventListener('scroll',function(){
+    let sec2 = document.querySelector(".item-2");
     let win = window.scrollY > 300;
     sec2.classList.toggle('sec-2-2-2',win)
 })
 
+window.addEventListener('scroll',function(){
+    let sec2 = document.querySelector(".item-3");
+    let win = window.scrollY > 300;
+    sec2.classList.toggle('sec-2-2-3',win)
+})
+
 
 window.addEventListener('scroll',function(){
-    let sec3 = document.querySelector(".sec-3");
+    let sec2 = document.querySelector(".item-4");
+    let win = window.scrollY > 300;
+    sec2.classList.toggle('sec-2-2-4',win)
+})
+
+//Sec-3-Animation-End
+
+
+//Sec-3-Animation-Start
+
+window.addEventListener('scroll',function(){
+    let sec3 = document.querySelector('.items-1-sec-3');
     let win = window.scrollY > 500;
-    sec3.classList.toggle('sec-3-3-3',win)
+    sec3.classList.toggle('sec-3-3-1-1',win)
 })
 
 
 
 window.addEventListener('scroll',function(){
-    let sec4 = document.querySelector(".sec-4");
+    let sec3 = document.querySelector('.items-1-sec-3-2');
+    let win = window.scrollY > 500;
+    sec3.classList.toggle('sec-3-3-2-2',win)
+})
+
+
+
+window.addEventListener('scroll',function(){
+    let sec3 = document.querySelector('.items-1-sec-3-3');
+    let win = window.scrollY > 500;
+    sec3.classList.toggle('sec-3-3-3-3',win)
+})
+
+//Sec-3-Animation-End
+
+
+
+
+//Sec-4-Animation-Start
+
+
+window.addEventListener('scroll',function(){
+    let sec4 = document.querySelector('.content-1');
     let win = window.scrollY > 1200;
-    sec4.classList.toggle('sec-4-4-4',win)
+    sec4.classList.toggle('sec-4-4-1',win)
+})
+
+
+window.addEventListener('scroll',function(){
+    let sec4 = document.querySelector('.special-img-1');
+    let win = window.scrollY > 1200;
+    sec4.classList.toggle('sec-4-4-2',win)
+})
+
+
+window.addEventListener('scroll',function(){
+    let sec4 = document.querySelector('.special-img-2');
+    let win = window.scrollY > 1200;
+    sec4.classList.toggle('sec-4-4-3',win)
+})
+
+//Sec-4-Animation-End
+
+
+//Sec-5-Animation-Start
+
+window.addEventListener('scroll',function(){
+    let sec5 = document.querySelector('.sec-5-content');
+    let win = window.scrollY > 1900;
+    sec5.classList.toggle('sec-5-5-1',win)
 })
 
 
 
 window.addEventListener('scroll',function(){
-    let sec5 = document.querySelector(".sec-5");
+    let sec5 = document.querySelector('.sec-5-img');
     let win = window.scrollY > 1900;
-    sec5.classList.toggle('sec-5-5-5',win)
+    sec5.classList.toggle('sec-5-5-2',win)
 })
+
+//Sec-5-Animation-End
